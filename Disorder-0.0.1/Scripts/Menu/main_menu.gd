e67e0a120010e9ff687b88d284b546aa93cc5823
Deva <juventusstark1221@gmail.com>
2026-09-04 02:05:58 +0530

    Wire Credits button to navigate to credits_menu.tscn

--- Disorder-0.0.1/Scripts/Menu/main_menu.gd
+++ Disorder-0.0.1/Scripts/Menu/main_menu.gd
@@ -4,10 +4,14 @@ extends CanvasLayer
 
 func _ready():
 	menu_draw.start_pressed.connect(_on_start)
+	menu_draw.credits_pressed.connect(_on_credits)
 	menu_draw.exit_pressed.connect(_on_exit)
 
 func _on_start():
 	get_tree().change_scene_to_file("res://Scenes/Menu/character_select.tscn")
+
+func _on_credits():
+	get_tree().change_scene_to_file("res://Scenes/Menu/credits_menu.tscn")
 
 func _on_exit():
 	get_tree().quit()
